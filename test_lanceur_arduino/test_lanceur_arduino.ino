8dca4e59dddd82d118b6e667b793771efaa4aef4
Becojo <benoit.cote.jodoin@gmail.com>
2016-12-29 20:57:53 -0500

    update launcher code

--- test_lanceur_arduino/test_lanceur_arduino.ino
+++ test_lanceur_arduino/test_lanceur_arduino.ino
@@ -1,99 +1,95 @@
 #include <Servo.h>
 
-const double LINEARSPEED = 3.50;  // m/s
-const double VERTANGLE = 40;  // angle in degrees 48 is max height, higher values will lower the angle
-
-double RADIUS = 0.0225; // radius of wheels in m
+//Constantes de la vitesse et de l'angle pour le lancer à effectuer selon la position
+const double SPEED = 4.82;  // m/s
+const double OFFSETSPEED = 0.0; // Ajustement de la vitesse provenant des algorithme -> en microSeconds
+const double VERTANGLE = 59.265;  // angle in degrees 61 is max height, 0 is lowest
 
 int pinServoVert = 3;
 int pinServoShoot = 6;
 
-const int shooterDown = 110;
-const int shooterUp = 40;
+Servo servoVert; //Servo pour modifier l'angle de tir
+Servo esc1;  // Moteur droite vue du tireur (vers la cible)
+Servo esc2;  // Moteur gauche vue du tireur (vers la cible)
+Servo shooter; //Servo pour pousser la balle vers les moteurs (pour tirer)
 
-Servo servoVert;
-Servo esc1;  // Moteur droit vue du tireur
-Servo esc2;  // Moteur gauche vue du tireur
-Servo shooter;
-
-double angleVertMax = 40.0;
-double angleVertMin = 20.0;
-
-int throttle1 = 0;
-int throttle2 = 0;
-
-const int vitessemax = 179;
-
-void setup() 
+void setup()
 {
-
   // put your setup code here, to run once:
-  servoVert.attach(pinServoVert);
-  shooter.attach(pinServoShoot);
-  esc1.attach(9);
-  esc2.attach(10);
-  //throttle1 = map(throttle1,0,1023,750,2400);
-  //throttle2 = map(throttle2,0,1023,750,2400);
-  esc1.writeMicroseconds(750);
-  esc2.writeMicroseconds(750); 
-  shooter.write(shooterUp);
-  delay(3000);
+  servoVert.attach(pinServoVert,900,2100); //Nombres = MIN et MAX du PWM (micro secondes) pour le serbo
+  shooter.attach(pinServoShoot,1500,2000);
+  esc1.attach(9); // Moteur droite attaché en PWM à la pin #9
+  esc2.attach(10); // Moteur droite attaché en PWM à la pin #9
+  esc1.writeMicroseconds(750); // Valeur minimal en micro secondes (PWM) à appliquer au moteur pour le bon fonctionnement
+  esc2.writeMicroseconds(750); // Valeur minimal en micro secondes (PWM) à appliquer au moteur pour le bon fonctionnement
+  shooter.writeMicroseconds(1500); // Position de base (bandé vers le ciel)
+  delay(3000); // Permet aux moteurs de s'initialiser (valeur à optimiser)
+  shooter.detach(); // Désalimente le shaft bandé du servo pour pousser la balle / évite de rentrer en conflit avec le servo de l'angle
 
 }
 
-
-double linSpeedToMicroSeconds(double vSpeed) 
+//Fonction permettant d'adapter la vitesse en m/s pour une consigne aux moteurs en micro secondes
+double linSpeedToMicroSeconds(double vSpeed)
 {
- double deltaY = 2400.0 - 750.0;
- double rmp_per_volts = 950;
- 
- const double pente = 1;
- double microSeconds = ((vSpeed * 60)/(2 * PI * RADIUS)) * (pente) + 750;
+ // y = 5*x + 885
+ const double pente = 5.0;
+ double microSeconds = (vSpeed * pente) + (885 + OFFSETSPEED);
  return microSeconds;
 }
 
+//Fonction permettant d'adapter l'angle voulue et celle réelle pour le servo
 double convertAngle(double pAngle)
 {
-  if(pAngle > angleVertMax)pAngle = angleVertMax;
-  if(pAngle < angleVertMin)pAngle = angleVertMin;
-
-  double pente = -1.1065;
-  double correctedAngle = pAngle * (pente) + 104.51;
-  return correctedAngle;
+   // y = -1.4367*x + 171.61
+  double pente = -1.4367;
+  double correctedAngle = pAngle * (pente) + 171.61;
+  double microSeconds = map (correctedAngle, 0,180,900,2100);
+  return microSeconds;
 }
 
-void raiseLauncher(double pAngle) 
+//Envoi la commande au servo de se position à l'angle voulue
+void raiseLauncher(double pAngle)
 {
-  // Raise vertical servo
-  servoVert.write(convertAngle(pAngle));
-  //servoVert.write(VERTANGLE);
+  servoVert.writeMicroseconds(convertAngle(pAngle));
 }
 
-
+////Envoi la commande aux servos des moteurs de se mettre à la vitesse voulue
 void activateLaunchWheels(double linearSpeed)
 {
-  //int rotSpeed = linSpeedToMicroSeconds(linearSpeed);
-  
-  esc1.writeMicroseconds(1475);
-  esc2.writeMicroseconds(1475);
-
+  double rotSpeed = linSpeedToMicroSeconds(linearSpeed); //Appel la fonction d'adaptation
+  esc1.writeMicroseconds(rotSpeed);
+  esc2.writeMicroseconds(rotSpeed);
 }
 
-void shoot()
+//Permet de pousser la balle
+void shootLauch()
 {
-  shooter.write(shooterDown);
+  shooter.attach(pinServoShoot,1500,2000); //Alimente le servo
   delay(1000);
-  shooter.write(shooterUp);
+  shooter.writeMicroseconds(1805); // 1805 -> position débandée du pousseux
+  delay(1000);
 }
 
-void loop() 
+//Remet en position pour être prêt à recevoir une balle
+void shootReturn()
+{
+  shooter.writeMicroseconds(1500); //Position bandée du pousseux
+  delay(1000);
+  shooter.detach(); //Désalimente le servo
+}
+
+void loop()
 {
   // put your main code here, to run repeatedly:
     raiseLauncher(VERTANGLE);
+    delay(1000);
+    activateLaunchWheels(SPEED);
     delay(2000);
-    //activateLaunchWheels(LINEARSPEED);
-    delay(1500);
-    shoot();
-    raiseLauncher(0);
-    delay(3000);
+    shootLauch();
+    esc1.writeMicroseconds(750); //Pour arrêter les moteurs
+    esc2.writeMicroseconds(750);
+    raiseLauncher(0); //Descends le lanceur
+    delay(1000);
+    shootReturn();
+    delay(2000);
 }
