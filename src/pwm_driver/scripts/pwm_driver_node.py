01a85baa0c60342cc212c84509bce4fa540125a6
Becojo <benoit.cote.jodoin@gmail.com>
2016-12-30 14:34:12 -0500

    handle pulse width with pwm_driver

--- src/pwm_driver/scripts/pwm_driver_node.py
+++ src/pwm_driver/scripts/pwm_driver_node.py
@@ -11,10 +11,16 @@ pwm = PWM(pi) # defaults to bus 1, address 0x40
 
 pwm.set_frequency(50)
 
-def handle_msg(msg):
+
+def handle_duty_cycle(msg):
     pwm.set_duty_cycle(msg.channel, msg.value)
 
-rospy.Subscriber('/duty_cycle', DutyCycle, handle_msg)
+
+def handle_pulse_width(msg):
+    pwm.set_pulse_width(msg.channel, msg.value)
+
+rospy.Subscriber('/duty_cycle', DutyCycle, handle_duty_cycle)
+rospy.Subscriber('/pulse_width', PulseWidth, handle_pulse_width)
 
 rospy.spin()
 
