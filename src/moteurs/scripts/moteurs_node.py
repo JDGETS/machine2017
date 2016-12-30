bf15f9b371c6d737e6134f0d731243b6a9a30630
machine 2017 <machine@jdgets.com>
2016-12-30 17:28:56 +0000

    update moteurs

--- src/moteurs/scripts/moteurs_node.py
+++ src/moteurs/scripts/moteurs_node.py
@@ -1,49 +1,45 @@
 #!/usr/bin/env python
 
 import rospy
-from pwm_driver.srv import SetDutyCycle
+from pwm_driver.msg import DutyCycle
 from geometry_msgs.msg import Twist
 
 rospy.init_node('moteurs')
 
 # PWM channels
-left_forward = rospy.get_param('~left_forward', 0)
-left_backward = rospy.get_param('~left_backward', 1)
-right_forward = rospy.get_param('~right_forward', 2)
-right_backward = rospy.get_param('~right_backward', 3)
+left_forward = rospy.get_param('/pins/motors_left_forward', 0)
+left_backward = rospy.get_param('/pins/motors_left_backward', 1)
+right_forward = rospy.get_param('/pins/motors_right_forward', 2)
+right_backward = rospy.get_param('/pins/motors_right_backward', 3)
+robot_width = rospy.get_param('~robot_width', 0.3)
+max_speed = rospy.get_param('~max_speed', 0.9)
 
 channels = [left_forward, left_backward, right_forward, right_backward]
 
-set_duty_cycle = rospy.ServiceProxy('set_duty_cycle', SetDutyCycle)
+pub = rospy.Publisher('/duty_cycle', DutyCycle, queue_size=10)
 
+def clamp(x, a, b):
+    return max(min(x, b), a)
 
 def handle_speed(forward, backward, speed):
-    if speed > 0:
-        set_duty_cycle(backward, 0)
-        set_duty_cycle(forward, 100)
+    if speed == 0:
+        pub.publish(DutyCycle(backward, 0))
+        pub.publish(DutyCycle(forward, 0))
+
+    elif speed > 0:
+        pub.publish(DutyCycle(backward, 0))
+        pub.publish(DutyCycle(forward, speed))
     else:
-        set_duty_cycle(backward, 100)
-        set_duty_cycle(forward, 0)
+        pub.publish(DutyCycle(backward, speed))
+        pub.publish(DutyCycle(forward, 0))
 
 
 def handle_cmd_vel(msg):
     speed = msg.linear.x
     angular = msg.angular.z
 
-    left_speed = 0
-    right_speed = 0
-
-    if abs(speed) > 0:
-        if speed > 0:
-            left_speed = 1
-            right_speed = 1
-        else:
-            left_speed = -1
-            right_speed = -1
-    else:
-        for channel in channels:
-            set_duty_cycle(channel, 0)
-        return
+    left_speed = clamp((speed - angular * robot_width * 0.5) / max_speed, -1, 1)
+    right_speed = clamp((speed + angular * robot_width * 0.5) / max_speed, -1, 1)
 
     handle_speed(left_forward_channel, left_backward_channel, left_speed)
     handle_speed(right_forward_channel, right_backward_channel, right_speed)
