8546b29c8c52c45b239395fa5851f96466490469
jdgetsmachine <log@clubcapra.com>
2016-12-31 13:27:11 -0500

    add test for louisf

---
+++ src/test_pwm_louif.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import pigpio
+
+# PWM, value defaults to 0, min to 0, max to 255
+# [GPIO, PWM <, value <, min <, max > > >]
+
+if __name__ == '__main__':
+    # pi.set_PWM_dutycycle()
+    pi = pigpio.pi()
+    pi.set_PWM_dutycycle(21, 25)
